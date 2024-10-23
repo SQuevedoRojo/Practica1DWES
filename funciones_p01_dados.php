7948956a58f420522d76686f6dd1e37aa1f457ed
asier <asier@gmail.com>
2024-10-23 09:52:30 +0200

    Recogida de jugadores y dados

--- funciones_p01_dados.php
+++ funciones_p01_dados.php
@@ -1,3 +1,35 @@
 <?php
+inicio();
+
+function inicio(){
+    $jugadores = compJugadores();
+    $numdados = compNumDados();
+}
+
+function compJugadores(){
+    $jugadores = array();
+    $cont = 1;
+    while ($cont < 5) {
+        $nom = "jug" . $cont;
+        if ($_REQUEST[$nom] != '')
+            $jugadores[$_REQUEST[$nom]] = array();
+        $cont++;
+    }
+    if($jugadores<2)
+        trigger_error("Numero de jugadores invalido", E_USER_ERROR);
+    else
+        return $jugadores;
+}
+
+function compNumDados(){
+    $numdados=0;
+    if ($_REQUEST["numdados"]<1 || $_REQUEST["numdados"]>10) {
+        trigger_error($_REQUEST["numdados"] > 10 ? "Numero superior a 10" : "Numero inferior a 1", E_USER_WARNING);
+        $numdados=$_REQUEST["numdados"]>10?10:1;
+    }else
+        $numdados=$_REQUEST["numdados"];
+    return $numdados;
+}
+
 
 ?>
--- p01_dados.php
+++ p01_dados.php
@@ -12,7 +12,7 @@
 
 <BODY>
 
-<form name='juegodados' action='' method=''>
+<form name='juegodados' action='funciones_p01_dados.php' method='post'>
 
 <div class="container ">
         <!--Aplicacion-->
